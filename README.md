0e44cdef304f7e21d759180d76362bfbed5c0b79
Gordon Hall <gordon@gordonwritescode.com>
2015-01-08 13:49:40 -0500

    add docs, moar tests, update coalescent version, cleanup

--- README.md
+++ README.md
@@ -1,4 +1,82 @@
-ipsee
+Ipsee
 =====
 
-inter-process communication for node
+[![Build Status](https://travis-ci.org/gordonwritescode/ipsee.svg)](https://travis-ci.org/gordonwritescode/ipsee)
+
+Ipsee facilitates IPC (Inter Process Communication) in Node.js via UNIX sockets.
+
+## Getting Started
+
+Install using NPM:
+
+```
+npm install ipsee --save
+```
+
+Give Ipsee a namespace for your application and subscribe:
+
+```js
+var ipsee = require('ipsee');
+var ipc   = ipsee('my_app_namespace').subscribe();
+```
+
+Now you can send and receive messages with your other processes and programs:
+
+```js
+ipc.on('ping', function(data) {
+  console.log('received ping from pid %d, sending pong...', data._fromUID);
+  ipc.send('pong');
+});
+```
+
+---
+
+## ipsee(namespace, [options])
+
+Creates a new UNIX socket server and connects to other sockets in the given namespace.
+
+### Methods
+
+#### i.subscribe([uid])
+
+If a `uid` is supplied, then subscribe to the socket server referenced, otherwise subscribe to all in the namespace.
+
+#### i.send(event, [data])
+
+Broadcast an event to other connected sockets containing optional data.
+
+#### i.close()
+
+End connections with all sockets and unlink the underlying file descriptor.
+
+### Options
+
+The `ipsee()` function's second argument is an *optional* `options` dictionary.
+
+#### path
+
+Directory path where UNIX socket file descriptor should be created. Default: `/tmp`.
+
+#### debug
+
+Print verbose debug messages to the console. Default: `false`.
+
+#### uid
+
+Unique identifier other ipsee instances use to refer to this one. Default: `process.pid`.
+
+### Events
+
+Aside from your custom event listeners, there are a few built in events.
+
+#### ready
+
+Emitted when this socket server is listening on the newly minted file descriptor.
+
+#### close
+
+Emitted when this socket server has been closed and the underlying file descriptor unlinked.
+
+#### error
+
+Emitted when something unexpected happens. Failure to handle this event will result in a `throw`.
--- test/messenger.unit.js
+++ test/messenger.unit.js
@@ -1,4 +1,4 @@
-var should = require('should');
+var expect = require('chai').expect;
 var ipsee = require('../lib/messenger');
 var fs = require('fs');
 var glob = require('glob');
@@ -15,43 +15,96 @@ after(function(done) {
 });
 
 describe('Messenger', function() {
-  
-  var ipc1, ipc2, ipc3;
-  
+
+  var ipc1, ipc2, ipc3, ipc4, ipc5;
+
   describe('@constructor', function() {
-    
-    it('should create the instance and setup a unix socket', function(done) {
-      ipc1 = ipsee('ipsee_test', { uid: 1 }).on('ready', function() {
-        should(fs.existsSync(ipc1.fd)).equal(true);
+
+    it('should not create an instance without a namespace', function(done) {
+      expect(ipsee).to.throw;
+      done();
+    });
+
+    it('should create the instance and setup socket with pid', function(done) {
+      ipc1 = ipsee('ipsee_test').on('ready', function() {
+        expect(fs.existsSync(ipc1.socketPath)).to.equal(true);
+        expect(ipc1.options.uid).to.equal(process.pid);
         done();
       });
     });
-    
+
+    it('should setup socket with ovewritten uid', function(done) {
+      ipc2 = ipsee('ipsee_test', { uid: 2 }).on('ready', function() {
+        expect(ipc2.options.uid).to.equal(2);
+        done();
+      });
+    });
+
   });
-  
+
   describe('#subscribe', function() {
-    
-    it('should create the instance and connect to socket', function(done) {
-      ipc2 = ipsee('ipsee_test', { uid: 2 }).on('ready', function() {      
-        ipc2.subscribe(1);
-        ipc2.channel.on('peerConnected', function() {
+
+    it('should subscribe to the socket with the given uid', function(done) {
+      ipc3 = ipsee('ipsee_test', { uid: 3 }).on('ready', function() {
+        ipc3.subscribe(2);
+        ipc2.channel.once('peerConnected', function() {
           done();
         });
       });
     });
-    
+
+    it('should subscribe to all existing sockets', function(done) {
+      var conns = 0;
+      ipc4 = ipsee('ipsee_test', { uid: 4 }).subscribe();
+      ipc1.channel.once('peerConnected', check);
+      ipc2.channel.once('peerConnected', check);
+      ipc3.channel.once('peerConnected', check);
+      function check() {
+        conns++;
+        if (conns === 3) done();
+      };
+    });
+
   });
-  
+
   describe('#send', function() {
-    
+
     it('should send the message and have it received', function(done) {
-      ipc1.once('test_event', function(data) {
-        should(data.beep).equal('boop');
+      ipc3.on('event1', function(data) {
+        expect(data.beep).to.equal('boop');
         done();
       });
-      ipc2.send('test_event', { beep: 'boop' });
+      ipc2.send('event1', { beep: 'boop' });
     });
-    
+
+    it('should include the sender uid in the data', function(done) {
+      ipc3.on('event2', function(data) {
+        expect(data._fromUID).to.equal(2);
+        done();
+      });
+      ipc2.send('event2');
+    });
+
+  });
+
+  describe('#close', function() {
+
+    it('should close the file descriptor when destroyed', function(done) {
+      ipc5 = ipsee('ipsee_test', { uid: 5 }).on('ready', function(data) {
+        ipc5.close();
+      }).on('close', function() {
+        expect(fs.existsSync(ipc5.socketPath)).to.equal(false);
+        done();
+      });
+    });
+
+    it('should get peerDisconnected event on peer destroy', function(done) {
+      ipc4.channel.once('peerDisconnected', done);
+      ipsee('ipsee_test', { uid: 5 }).on('ready', function(data) {
+        this.close();
+      }).subscribe(4);
+    });
+
   });
 
 });
